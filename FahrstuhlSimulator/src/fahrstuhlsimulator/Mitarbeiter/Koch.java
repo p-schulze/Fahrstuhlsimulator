b4da518939ee82c1f9713c72b8f77f41ed7c9462
GFan99 <sj.beck@web.de>
2016-01-27 21:53:01 +0100

    Koch, Mitarbeiter und leere Etage eingefügt

---
+++ FahrstuhlSimulator/src/fahrstuhlsimulator/Mitarbeiter/Koch.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fahrstuhlsimulator.Mitarbeiter;
+
+import java.util.ArrayList;
+import fahrstuhlsimulator.Gebaeude.Etage;
+
+/**
+ *
+ * @author becksusanna
+ */
+public class Koch extends Mitarbeiter{
+    protected Koch(int id, String name) {
+        super(id,name);
+        ArrayList<Etage> liste = new ArrayList();
+        //liste.add(EG);
+        //liste.add(Cafeteria);
+        setErlaubteEtagen(liste);
+    }
+}
---
+++ FahrstuhlSimulator/src/fahrstuhlsimulator/Mitarbeiter/Mitarbeiter.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fahrstuhlsimulator.Mitarbeiter;
+
+import java.util.ArrayList;
+import fahrstuhlsimulator.Gebaeude.Etage;
+
+/**
+ *
+ * @author becksusanna
+ */
+public class Mitarbeiter {
+    private int id;
+    private String name;
+    private Etage aktuelleEtage;
+    private ArrayList<Etage> erlaubteEtagen;
+    
+    protected Mitarbeiter(int id, String name) {
+        this.id=id;
+        this.name=name;
+        //this.erlaubteEtagen=erlaubteEtagen;
+        //this.aktuelleEtage=EG;
+    }
+    
+    protected int getID() {
+        return id;
+    }
+    protected void setID(int id) {
+        this.id=id;
+    }
+    
+    protected String getName() {
+        return name;
+    }
+    protected void setName(String name) {
+        this.name=name;
+    }
+    
+    protected Etage getAktEtage() {
+        return aktuelleEtage;
+    }
+    protected void setAktEtage(Etage aktEtage) {
+        this.aktuelleEtage=aktEtage;
+    }
+    
+    protected ArrayList<Etage> getErlaubteEtagen() {
+        return erlaubteEtagen;
+    }
+    protected void setErlaubteEtagen(ArrayList<Etage> nErlaubteEtagen) {
+        this.erlaubteEtagen=nErlaubteEtagen;
+    }
+}
